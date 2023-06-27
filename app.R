b4ced773b370557b9e35ee4a6bd6e6b156dd6f5f
gokhanhimself <106489173+gokhanhimself@users.noreply.github.com>
2023-06-27 13:50:19 +0300

    Update app.R

--- app.R
+++ app.R
@@ -34,9 +34,9 @@ ui <- fluidPage(
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+                  min = 11,
+                  max = 31,
+                  value = 21)
       
     ),
     
